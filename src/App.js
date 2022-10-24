3eb777884afd28cb7a1674a8b75511865cc27f8c
BanhmiPate1311 <huynhcuong@gmail.com>
2022-10-24 10:29:51 +0700

    upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import { BrowserRouter } from "react-router-dom";
+import Routers from "./routers/Routers";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routers />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
